b94abc754aa670214feaedcd395ca0408fc8e86d
Alex Blackie <alex@alexblackie.com>
2014-05-31 18:09:27 -0700

    Add port clarification to console output
    I forgot what port it was starting on, so this should help me not
    forget.

--- bin/server.sh
+++ bin/server.sh
@@ -5,6 +5,6 @@ GIT_ROOT=`git rev-parse --show-toplevel`
 (
   cd "$GIT_ROOT/www"
 
-  echo "Starting SimpleHTTPServer in `pwd`"
+  echo "Starting SimpleHTTPServer in `pwd` at 0.0.0.0:8000"
   python -m SimpleHTTPServer 8000
 )
