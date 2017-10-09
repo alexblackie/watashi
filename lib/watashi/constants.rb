95054c3867b9cefd7800f038b66a70289fb0f7b6
Alex Blackie <alex@alexblackie.com>
2017-10-09 16:43:24 +0900

    Combine albums index and show
    Having photosets listed on a separate page was stupid. Just list them in
    the album index, and get rid of album permalinks altogether; they served
    no purpose.

--- lib/watashi/constants.rb
+++ lib/watashi/constants.rb
@@ -9,7 +9,6 @@ module Watashi
     %r{^/articles/(?<slug>.+)$} => {class: "Watashi::Controllers::ArticlesController", methods: ["GET"]},
 
     %r{^/albums/?$} => { class: "Watashi::Controllers::AlbumsController", methods: ["GET"]},
-    %r{^/albums/(?<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/?$} => {class: "Watashi::Controllers::AlbumController", methods: ["GET"]},
     %r{^/albums/(?<album_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/(?<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/?$} => {class: "Watashi::Controllers::PhotoSetController", methods: ["GET"]},
 
 
