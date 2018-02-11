1d922fcff52eadb294cf7af60dc0366b9472cdc2
Alex Blackie <alex@alexblackie.com>
2018-02-11 01:45:58 -0500

    Add a nice 404 page
    Since we're sideloading meta now, it would fail if the page didn't
    exist, so we need a manual error handler branch in here.

--- lib/watashi/controllers/pages_controller.rb
+++ lib/watashi/controllers/pages_controller.rb
@@ -6,6 +6,13 @@ module Watashi
         slug = @captures[:slug]
         meta = Watashi::Services::DataBag.new(model: Watashi::Domain::Page).one(slug)
 
+        if meta.nil?
+          return respond(code: 404, template: "pages/404", context: {
+            page_title: "HTTP 404: Resource Not Found",
+            stylesheets: ["error"]
+          })
+        end
+
         respond(template: "pages/#{slug}", context: {
           page_title: meta.title,
           body_class: "page-#{slug}",
